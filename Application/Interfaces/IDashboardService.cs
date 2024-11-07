c56e78f023358bf1cc259527a6d4cb8ec2509fdf
Juan Martin <j.masqueda@gmail.com>
2024-11-07 18:05:59 -0300

    Agrego controllador y dtos para dashboard

---
+++ Application/Interfaces/IDashboardService.cs
@@ -0,0 +1,14 @@
+﻿using Application.Models.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IDashboardService
+    {
+        DashboardSummaryDto GetDashboardSummary(int adminId);
+    }
+}
---
+++ Application/Services/DashboardService.cs
@@ -0,0 +1,67 @@
+﻿using Application.Interfaces;
+using Application.Models.Responses;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class DashboardService : IDashboardService
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IOrderRepository _orderRepository;
+        private readonly IClientRepository _clientRepository;
+
+        public DashboardService(
+            IProductRepository productRepository,
+            IOrderRepository orderRepository,
+            IClientRepository clientRepository)
+        {
+            _productRepository = productRepository;
+            _orderRepository = orderRepository;
+            _clientRepository = clientRepository;
+        }
+
+        public DashboardSummaryDto GetDashboardSummary(int adminId)
+        {
+            // Total de productos del admin
+            var totalProducts = _productRepository.GetAllByAdmin(adminId).Count;
+
+            // Total de órdenes (puedes ajustar según tu lógica de negocio)
+            var totalOrders = _orderRepository.GetAll().Count;
+
+            // Total de clientes
+            var totalClients = _clientRepository.GetAll().Count;
+
+            // Calcular ingresos totales
+            var totalRevenue = _orderRepository.GetAll().Sum(o => o.Total);
+
+           
+
+            // Órdenes recientes
+            var recentOrders = _orderRepository.GetAll()
+                .OrderByDescending(o => o.OrderDate)
+                .Take(5)
+                .Select(o => new RecentOrderDto
+                {
+                    OrderId = o.Id,
+                    ClientName = o.Client.Name,
+                    OrderDate = o.OrderDate,
+                    Total = o.Total
+                })
+                .ToList();
+
+            return new DashboardSummaryDto
+            {
+                TotalProducts = totalProducts,
+                TotalOrders = totalOrders,
+                TotalClients = totalClients,
+                TotalRevenue = totalRevenue,
+                RecentOrders = recentOrders
+            };
+        }
+    }
+}
